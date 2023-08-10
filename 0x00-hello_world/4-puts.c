01f49fd017d819b4e5b16b72a44b1f6c605e3ee1
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-10 16:18:47 -0700

    task 3-4 complete

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/*
+  * main function  is to print hello
+  * return is an integer 0 meaning success
+*/
+#include<stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
