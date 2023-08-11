29dfc7b28b75dc4966403f25eac637d2dbb00712
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-10 17:16:32 -0700

    task 5-6 complete

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/*
+   *Program with printf function
+   *main returns integer 0 but takes no argument i,e void
+ */
+#include<stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
