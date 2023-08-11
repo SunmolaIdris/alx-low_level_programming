8ddf176c9cfd08a7b7285269e68473aaa386a176
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-10 17:51:35 -0700

    correction of comment line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
+#include<stdio.h>
 /**
   * main function  is to print hello
   * return is an integer 0 meaning success
   */
-#include<stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
