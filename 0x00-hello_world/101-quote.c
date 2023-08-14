c5d5d153ca9304fb0d27976b4443bf59f0c0dc76
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-14 07:04:23 -0700

    task 101 complete

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - main function in the task 101 for hello world
+*printing without the use of put and printf i.e using write function and that includes using unistd.h library
+*
+*
+*Return: return 1 for this program
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
