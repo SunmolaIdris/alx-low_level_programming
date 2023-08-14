bb246275c8eca2126e6956a5646e540a3c792927
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-14 05:37:26 -0700

    task 4 complete

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - Main function of my project
+*
+*
+*Return: Always return 0 everytime
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
