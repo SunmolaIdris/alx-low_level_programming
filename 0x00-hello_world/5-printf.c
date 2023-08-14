2964e90b8ed9e6c3a2647ed5125bb60a0f459767
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-14 06:06:33 -0700

    task 5 complete

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
-*main - main function of my program with printf function
-*Return - returns integer 0 always
+*main - Main function of my printf project
+*
+*
+*Return: return 0 always
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
