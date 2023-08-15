5e6e8c341a0b84975fab3f303b1ac95f99916694
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-15 16:16:52 -0700

    complete task 0 and 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - is the main function for task 0.
+* Prints "_putchar" followed by a new line.
+* return - Returns 0 to indicate successful execution
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point for not a main function
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
