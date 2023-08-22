01d9b73f9336369cbc23cf7b5b688c8a8f00b3c5
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-21 22:01:44 -0700

    task 7 retry

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,13 +7,21 @@
  * @a: array to be printed
  * @n: number of elements in the array
  */
+
 void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
 	}
-		printf("\n");
+	putchar('\n');
 }
