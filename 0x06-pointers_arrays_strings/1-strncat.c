e35acd72eb7e5c1694049a8c03253646a1c38238
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-24 14:42:06 -0700

    complete task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
- * _strncat - Fxn concatenates n bytes from a string to another
- * @dest: destination string
+ * _strncat -  Function joins n bytes from a string to another
+ * @dest: Destination string
  * @src: source string
- * @n: number of bytes of str to concatenate
+ * @n: number of bytes of string to concatenate
  *
  * Return: a pointer to the resulting string dest
  */
@@ -15,21 +14,22 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (src[j] != '\0' && j<n)
+	j = 0;
+
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
+
 	}
 
-	dest[i] = '\0';
 
 	return (dest);
 }
