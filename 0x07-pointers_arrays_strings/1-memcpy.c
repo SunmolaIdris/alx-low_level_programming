61bea9c8776e4f5a648a6cafe233961b8aac92ef
SunmolaIdris <sunmolaidris@gmail.com>
2023-08-28 20:21:54 -0700

    task 1 complete

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: buffer where we will copy to
+ * @src: memory area to copy from
+ * @n: n bytes of @src
+ *
+ * Return: Always 0 (Success)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (k = 0; k < n; k++)
+		dest[k] = src[k];
+	return (dest);
+}
