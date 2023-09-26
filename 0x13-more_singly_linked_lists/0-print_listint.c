baf2643ba6254aaf5fe0975216a093aa130d9f6c
SunmolaIdris <sunmolaidris@gmail.com>
2023-09-26 13:09:08 -0700

    complete task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t length = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		length++;
 		h = h->next;
 	}
 
-	return (num);
+	return (length);
 }
 
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the 
+ * number of elements in a linked list.
+ * @h: linked list of typelistint_t to go through
+ *
+ * size_t: number of nodes to return 
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t length = 0;
+
+	while (h)
+	{
+		length++;
+		h = h->next;
+	}
+
+	return (length);
+}
