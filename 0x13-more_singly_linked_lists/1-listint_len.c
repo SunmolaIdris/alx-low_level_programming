2a26cfd8a719de13faccb17604179b2c34533e20
SunmolaIdris <sunmolaidris@gmail.com>
2023-09-26 13:20:43 -0700

    Betty check

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * listint_len - function that returns the 
+ * listint_len - function that returns the
  * number of elements in a linked list.
  * @h: linked list of typelistint_t to go through
  *
- * size_t: number of nodes to return 
+ * Return - size_t: number of nodes to return
  */
 
 size_t listint_len(const listint_t *h)
